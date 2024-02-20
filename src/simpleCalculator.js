65274a07e609bd55c3f5fe876c699da869cdce8a
Mahammad Anis Vahora <anisvahora00@gmail.com>
2024-02-19 19:05:31 -0500

    syntax error - fix

--- src/simpleCalculator.js
+++ src/simpleCalculator.js
@@ -1,21 +1,20 @@
 function add (a, b) {
-  return a + b
+  return a + b;
 }
 
 function subtract (a, b) {
-  return a - b
+  return a - b;
 }
 
 function multiply (a, b) {
-  return a * b
+  return a * b;
 }
 
 function divide (a, b) {
   if (b === 0) {
-    throw new Error('Invalid Input! 
-    Cannot divide by zero')
+    throw new Error('Invalid Input! Cannot divide by zero');
   }
-  return a / b
+  return a / b;
 }
 
 module.exports = {
