caa593f3402cca6ace35de70fbbba525a5e033a1
Mahammad Anis Vahora <anisvahora00@gmail.com>
2024-02-19 21:30:13 -0500

    task 7 demo

--- src/simpleCalculator.test.js
+++ src/simpleCalculator.test.js
@@ -12,8 +12,8 @@ test('multiplies 3 * 2 to equal 6', () => {
 	expect(multiply(3, 2)).toBe(6);
 });
 
-test('divides 10 / 2 to equal 5', () => {
-	expect(divide(10, 2)).toBe(5);
+test('divides 12 / 2 to equal 6', () => {
+	expect(divide(12, 2)).toBe(6);
 });
 
 test('throws error when dividing by zero', () => {
