157e0e158fc800e05ea32afc1023ba3b1d6bce41
Mahammad Anis Vahora <anisvahora00@gmail.com>
2024-02-19 19:48:22 -0500

    adding style check

--- src/simpleCalculator.js
+++ src/simpleCalculator.js
@@ -11,12 +11,8 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-
-  
   if (b === 0) {
-    throw new Error(
-      "Invalid Input!
-       Cannot divide by zero");
+    throw new Error("Invalid Input! Cannot divide by zero");
   }
   return a / b;
 }
